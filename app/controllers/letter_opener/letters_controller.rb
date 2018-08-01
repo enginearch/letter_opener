d34aef983d2544f845d5fffe360ea60e4ba8d307
Michal Krakowiak <michal.krakowiak@enginearch.com>
2018-08-01 15:30:26 +0200

    Fixed(?) rendering mails

--- app/controllers/letter_opener/letters_controller.rb
+++ app/controllers/letter_opener/letters_controller.rb
@@ -6,7 +6,9 @@ module LetterOpener
 
     def show
       @letter = Letter.find_by_name(params[:id])
-      render html: @letter.contents(params[:style])
+      respond_with do |format|
+       format.html { render @letter.contents(params[:style]) }
+      end
     end
   end
 end
